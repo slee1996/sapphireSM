94a79c28a5cbcfa732947773b7538fbd8afe7236
slee1996 <spencerlee96@gmail.com>
2023-11-09 09:45:02 -0700

    chore: metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,8 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Dalle3 App',
-  description: 'Playground time :)',
+  title: 'SapphireSM',
+  description: 'Let your imagination run wild!',
 }
 
 export default function RootLayout({
