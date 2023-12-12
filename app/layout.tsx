d272610a940d4e5e68e0e300c95d3dd582f201ae
slee1996 <spencerlee96@gmail.com>
2023-12-12 12:01:41 -0700

    fix: app metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "SapphireSM",
-  description: "Let your imagination run wild!",
+  title: "Thumbnailr",
+  description: "Elevate your YouTube presence with Thumbnailr, the innovative app powered by the groundbreaking AI of DALL-E 3 and DALL-E 2. Unleash your creativity and generate eye-catching, custom thumbnails with ease. Thumbnailr harnesses advanced AI to transform your ideas into visually stunning designs, ensuring your videos stand out in the crowded digital landscape. Experience effortless thumbnail creation and captivate your audience at first glance!"
 };
 
 export default function RootLayout({
